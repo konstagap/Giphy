7fd647eed35a165188c2aea0881ffbb0cabb7da4
konstagap <konstagapov123@gmail.com>
2019-09-12 00:06:00 -0700

    fix bugs

--- javascript.js
+++ javascript.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
             button.attr("data-name", sports[i])
             button.attr("class","button")
             $(".buttons").append(button)
-            console.log(button.attr("data-name"))
+            // console.log(button.attr("data-name"))
             // $(".buttons").append($("<button>").html(sports[i]).attr("id", "giphyButton").attr("data-name", sports[i]))
         }
     }
@@ -19,32 +19,36 @@ $(document).ready(function () {
     // when we press #submitbutton we creat new var and assign value of usersearch in in, then we push it to our array
     // // we also use showbuttons function to update array with new value and add it to screen as button.
     $(document).on("click", "#submitButton", function (event) {
+        if ($("#userSearch").val() == ""){
+            return
+        } else{
         event.preventDefault();
         var newSport = $("#userSearch").val();
-        console.log(newSport)
         $("#userSearch").val("")
         sports.push(newSport)
         showButtons();
-        // console.log(sports[5])
+        }
+        
     });
     // // all our queries in buttons, when we press button we take its dataname and use it for our ajax request.
     // //we assing dataname to var and add it to url for ajax request
     $(document).on("click", "button", function () {
+
         $("#addGif").empty()
         var userInput = $(this).attr("data-name")
-        console.log(userInput)
+        // console.log(userInput)
         var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=LtSt5fhdrBCp8mAHlmkMKdduM0RTj5iC&q=" + userInput + "&limit=10&offset=0&rating=G&lang=en"
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function (response) {
-            console.log(response)
+            // console.log(response)
             var results = response.data;
             for (i = 0; i < 10; i++) {
                 var gifBox = $("<div>");
                 gifBox.attr("class","float-left")
                 var rating = $("<p>").text("Rating: " + results[i].rating);
-                console.log(rating)
+                // console.log(rating)
 
             //adding many attr to image for pausing effect on gifs, as its loaded it is still 
                 var image = $("<img>").attr("src", results[i].images.fixed_height_still.url)
@@ -55,7 +59,7 @@ $(document).ready(function () {
                 gifBox.append(rating)
                 gifBox.append(image)
                 $("#addGif").prepend(gifBox)
-                console.log(image.attr("data-state"))
+                // console.log(image.attr("data-state"))
             }
         })
 
@@ -64,7 +68,7 @@ $(document).ready(function () {
     // //to data animate attr which got link for animated gif. second click gonna return to still
     $(document).on("click", ".gif", function () {
         var state = $(this).attr("data-state")
-        console.log(state)
+        // console.log(state)
         if (state === "still") {
             $(this).attr("src", $(this).attr("data-animate"));
             $(this).attr("data-state", "animate");
